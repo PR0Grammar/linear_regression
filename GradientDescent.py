6db2cb07ac43130edc03ba3625c5c3ceaf06cec4
Pr0grammar <npatel3030@gmail.com>
2018-12-27 20:21:04 -0500

    gradient descent method added. Predicted values tested

--- GradientDescent.py
+++ GradientDescent.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 class GradientDescent:
-    def __init__(self, X, y, alpha = 0.01):
+    def __init__(self, X, y, alpha = 0.01, iterations = 1500):
         self.alpha = alpha
+        self.iterations = iterations
         self.m = X.shape[0]
         self.n = X.shape[1] + 1
         self.thetas = np.zeros((X.shape[1] + 1, 1))
@@ -18,10 +19,35 @@ class GradientDescent:
         theta_transposed = self.thetas.transpose()
         prediction_err = 0
         for i in range(0, self.m):
-            x = self.features[i: i + 1, 0:self.n]
+            x = self.features[i: i + 1, 0: self.n]
             x = x.transpose()
             predicted_val = np.matmul(theta_transposed, x)[0, 0]
             prediction_err = prediction_err + pow((predicted_val - self.results[i, 0]), 2)
         
         prediction_err = (1.0 / (2.0 * self.m)) * prediction_err
         return prediction_err
+
+    # Perform gradient descent to minimize cost
+    def gradient_descent(self):
+        for i in range (0, self.iterations):
+            print('J(Theta): ' + str(self.compute_cost()))
+
+            updated_theta_values = []
+            theta_transposed = self.thetas.transpose()
+
+            for j in range(0, self.thetas.shape[0]):
+                theta_j = self.thetas[j, 0] 
+                err_sum = 0
+                for k in range(0, self.m):
+                    x = self.features[k: k+1, 0: self.n]
+                    x = x.transpose()
+                    predicted_val = np.matmul(theta_transposed, x)[0,0]
+                    predicted_val = (predicted_val - self.results[k, 0]) * self.features[k, j]
+                    err_sum = err_sum + predicted_val
+
+                new_theta_j = (self.alpha / self.m) * err_sum
+                new_theta_j = theta_j - new_theta_j
+                updated_theta_values.append(new_theta_j)
+
+            for j in range(0, self.thetas.shape[0]):
+                self.thetas[j, 0] = updated_theta_values[j]
--- single_variable_plot.py
+++ single_variable_plot.py
@@ -14,4 +14,13 @@ plt.plot()
 plt.show()
 
 lin_reg = GradientDescent(x, y)
+lin_reg.gradient_descent()
 
+predict_one = np.matmul(np.array([1, 15.0]), lin_reg.thetas)
+predict_two = np.matmul(np.array([1, 13.0]), lin_reg.thetas)
+predict_three = np.matmul(np.array([1, 4.7]), lin_reg.thetas)
+
+
+print('Prediction for x = 15.0: ' + str(predict_one[0]))
+print('Prediction for x = 13.0 ' + str(predict_two[0]))
+print('Prediction for x = 4.7 ' + str(predict_three[0]))
