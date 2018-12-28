a2f02ae3a6c036a27240483766ac6acddd45517a
Pr0grammar <npatel3030@gmail.com>
2018-12-27 20:45:59 -0500

    Added hypothesis function for single variable plot

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Linear Regression
 
 Practice implementing linear regression through gradient descent and normal equation. Data provided from Andrew Ng's Machine Learning course on Coursera.
+
+### Gradient descent with single feature/variable
+![Single Variable Graph](plot_graphs/single_var_graph.png)
--- single_variable_plot.py
+++ single_variable_plot.py
@@ -7,20 +7,23 @@ data_matrix = np.loadtxt('./exdata1.txt')
 x = data_matrix[0:data_matrix.shape[0], 0:1]
 y = data_matrix[0:data_matrix.shape[0], 1:2]
 
+
+lin_reg = GradientDescent(x, y) 
+lin_reg.gradient_descent()
+
+predicted_x = []
+predicted_y = []
+
+i = 0;
+
+while(i < 23.50):
+    predicted_x.append(i)
+    hypoth_of_i = np.matmul([1, i], lin_reg.thetas)[0]
+    predicted_y.append(hypoth_of_i)
+    i = i + 0.5
 plt.plot(x, y, 'ro')
+plt.plot(predicted_x, predicted_y)
 plt.ylabel('Profit in $10,000s')
 plt.xlabel('Population of City in 10,000s')
 plt.plot()
 plt.show()
-
-lin_reg = GradientDescent(x, y)
-lin_reg.gradient_descent()
-
-predict_one = np.matmul(np.array([1, 15.0]), lin_reg.thetas)
-predict_two = np.matmul(np.array([1, 13.0]), lin_reg.thetas)
-predict_three = np.matmul(np.array([1, 4.7]), lin_reg.thetas)
-
-
-print('Prediction for x = 15.0: ' + str(predict_one[0]))
-print('Prediction for x = 13.0 ' + str(predict_two[0]))
-print('Prediction for x = 4.7 ' + str(predict_three[0]))
